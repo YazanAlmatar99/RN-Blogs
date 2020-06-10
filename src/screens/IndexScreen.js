55591f984224469d33d53bca830555bde86eb49f
Yazan <yazanmatar90@gmail.com>
2020-06-10 17:42:06 -0400

    updated index to accept the refactor

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,19 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { Text, View, StyleSheet, FlatList, Button } from "react-native";
+import { Context } from "../context/BlogContext";
 
 const IndexScreen = () => {
+  const { state, addBlogPost } = useContext(Context);
   return (
     <View>
-      <Text>IndexScreen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blog) => blog.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
